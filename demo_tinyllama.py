d5b2a80a35917fe13aaa43be4eae188e7fe2f76f
Mallik Gurram <mallikgarjuna@gmail.com>
2025-10-23 05:59:08 +0200

    Test Ollama chat response

---
+++ demo_tinyllama.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+from ollama import ChatResponse, chat
+
+MODEL = "tinyllama"
+
+response: ChatResponse = chat(
+    model=MODEL,
+    messages=[{"role": "user", "content": "Why is the sky blue? In one sentence."}],
+    stream=False,
+)
+
+pprint(response)
+pprint(response.model_dump())
+for key in response.model_dump():
+    print(key)
+
+type(response)
+type(response.message)
+
+pprint(response.model_dump_json())
+pprint(response.message.model_dump())
+pprint(response.message.model_dump_json())
+# print(response)
+print(response.message.content)
