150be9e15d6b6c2c363a1528fc3cd7afbbbdf004
Mallik Gurram <mallikgarjuna@gmail.com>
2025-10-24 07:24:39 +0200

    ollama-python package chat - handle ollama exceptions

--- 5_package_chat.py
+++ 5_package_chat.py
@@ -4,7 +4,7 @@
 import ollama
 from rich import print as rich_print
 
-model = "tinyllama"
+model = "tinyllamax"
 
 messages = [
     {
@@ -19,7 +19,14 @@ messages = [
 # rich_print(response_chat.message.content)
 
 # Steam chat ===============
-response_chat_stream = ollama.chat(model=model, messages=messages, stream=True)
+try:
+    response_chat_stream = ollama.chat(model=model, messages=messages, stream=True)
 
-for part in response_chat_stream:
-    print(part.message.content, end="", flush=True)
+    for chunk in response_chat_stream:
+        print(chunk.message.content, end="", flush=True)
+except ollama.RequestError as exc:
+    rich_print(f"[yellow]An error occured while requesting:[/yellow] {exc.error}")
+except ollama.ResponseError as exc:
+    rich_print(f"[red]An error occured in response:[/red] {exc.error}")
+except Exception as exc:
+    rich_print(f"[red]Unexpected error:[/red] {exc}")
