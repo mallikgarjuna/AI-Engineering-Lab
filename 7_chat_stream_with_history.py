81fd2302763ffaed53b058df312566797c4862f8
Mallik Gurram <mallikgarjuna@gmail.com>
2025-10-24 11:11:44 +0200

    ollama-python chat stream with message history - refactor and add is_ollama_running()

--- 7_chat_stream_with_history.py
+++ 7_chat_stream_with_history.py
@@ -1,46 +1,87 @@
 # 7_chat_stream_with_history.py
 
 import pprint
+import sys
+from json import JSONDecodeError
 from typing import Any
 
+import httpx
 import ollama
 
-model = "tinyllama"
-message_history: list[dict[str, Any]] = []
 
-while True:
-    user_input = input("User input: ")
-    if user_input.lower() in ["exit", "stop", "quit"]:
-        print("Chat ending...")
-        break
+# Test whether local Ollama server is running
+# before running this code
+def is_ollama_running() -> bool:
+    host = "http://localhost:11434"
+    endpoint = "/api/version"
+    url = host + endpoint
 
-    user_message = {
-        "role": "user",
-        "content": user_input,
-    }
+    try:
+        response = httpx.get(url=url)
+    except httpx.InvalidURL as exc:
+        print(f"Invalid URL error: {exc}")
+        return False
+    except httpx.RequestError as exc:
+        print(f"Server/network error while accessing: {exc.request.url}")
+        print("Make sure to run Ollama locally with `ollama serve`")
+        return False
+    else:
+        try:
+            data = response.json()
+        except JSONDecodeError as exc:
+            print(f"Error while parsing JSON data to py obj: {exc.msg}")
+        else:
+            print(
+                f"Ollama version: {data['version']} network response: {response.status_code}"
+            )
+        return response.status_code == httpx.codes.OK  # instead of hardcoding 200;
 
-    message_history += [user_message]
 
-    response_chat_stream = ollama.chat(
-        model=model,
-        messages=message_history,
-        stream=True,
-    )
+# Chat stream
+def chat_stream() -> None:
+    model = "tinyllama"
+    message_history: list[dict[str, Any]] = []
 
-    assistant_reply = ""
+    while True:
+        user_input = input("User input: ")
+        if user_input.lower() in ["exit", "stop", "quit"]:
+            print("Chat ending...")
+            break
 
-    for chunk in response_chat_stream:
-        if not chunk.message.content:
-            continue
+        user_message = {
+            "role": "user",
+            "content": user_input,
+        }
 
-        assistant_reply += chunk.message.content
-        print(chunk.message.content, end="", flush=True)
-    print()
+        message_history += [user_message]
 
-    assistant_message = {
-        "role": "assistant",
-        "content": assistant_reply,
-    }
+        response_chat_stream = ollama.chat(
+            model=model,
+            messages=message_history,
+            stream=True,
+        )
 
-    message_history += [assistant_message]
-    pprint.pprint(message_history, sort_dicts=False)
+        assistant_reply = ""
+
+        for chunk in response_chat_stream:
+            if not chunk.message.content:
+                continue
+
+            assistant_reply += chunk.message.content
+            print(chunk.message.content, end="", flush=True)
+        print()
+
+        assistant_message = {
+            "role": "assistant",
+            "content": assistant_reply,
+        }
+
+        message_history += [assistant_message]
+        pprint.pprint(message_history, sort_dicts=False)
+
+
+if __name__ == "__main__":
+    if not is_ollama_running():
+        sys.exit()
+    else:
+        chat_stream()
