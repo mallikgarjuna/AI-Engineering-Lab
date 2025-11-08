3fd341a8eb05603cac4b85985f4defab3b82656a
Mallik Gurram <mallikgarjuna@gmail.com>
2025-11-08 15:36:50 +0100

    AIEng/2-LangChain - l2 chains and agents - sequential chains with LCEL

--- 12_LangChain/l2_chains_and_agents.py
+++ 12_LangChain/l2_chains_and_agents.py
@@ -8,6 +8,13 @@
 # Sequential problems
 
 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def building_prompt_for_sequential_chains():
     from langchain_core.prompts import PromptTemplate
 
@@ -31,6 +38,52 @@ def building_prompt_for_sequential_chains():
 
 # With these prompts set up, you're now to set up the LLM and chain them
 # all together using LCEL.
+# LECL = LangChain Expression Language
+
+
+def sequential_chains_with_LCEL():
+    from langchain_core.output_parsers import StrOutputParser
+    from langchain_core.prompts import PromptTemplate
+    from langchain_openai import ChatOpenAI
+
+    learning_activity_template = PromptTemplate(
+        template="I want to learn how to {activity}. Can you suggest how I can learn this step-by-step?",
+        input_variables=["activity"],
+    )
+
+    time_plan_template = PromptTemplate(
+        template="I only have one week. Can you create a concise plan to help me hit this goal: {learning_plan}",
+        input_variables=["learning_plan"],
+    )
+
+    llm = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4o-mini",
+        max_completion_tokens=200,
+    )
+
+    # Complete the sequential chain with LCEL:
+    # llm → generates an AIMessage
+    # StrOutputParser() → extracts the .content as string
+    # Without StrOutputParser(): You’d get an AIMessage object — not a string, breaking the next prompt.
+    seq_chain = (
+        {"learning_plan": learning_activity_template | llm | StrOutputParser()}
+        | time_plan_template
+        | llm
+        | StrOutputParser()
+    )
+
+    # Call the chain
+    response = seq_chain.invoke(input={"activity": "play the piano"})
+
+    print(response)
+
+
+# Running a series of chains opens up lots of possibility for designing more
+# sophisticated workflows. Now you're beginning to grasp how LangChain can
+# handle more sophisticated workflows, it's time to talk about agents,
+# which enable LLMs to make decisions.
 
 if __name__ == "__main__":
-    building_prompt_for_sequential_chains()
+    # building_prompt_for_sequential_chains()
+    sequential_chains_with_LCEL()
