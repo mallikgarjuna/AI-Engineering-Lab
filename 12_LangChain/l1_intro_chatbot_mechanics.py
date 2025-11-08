a3ea24d21562972205362a9114e338e7e8d25aea
Mallik Gurram <mallikgarjuna@gmail.com>
2025-11-08 08:14:55 +0100

    AIEng/2-LangChain - l1 intro and chatbot mechanics - huggingface  models in langchain

--- 12_LangChain/l1_intro_chatbot_mechanics.py
+++ 12_LangChain/l1_intro_chatbot_mechanics.py
@@ -1,18 +1,21 @@
 import os
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-llm = ChatOpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-    model="gpt-4o-mini",
-    max_completion_tokens=100,
-)
-
 
 def openai_models_in_langchain():
+    # Install: uv add langchain
+    # Install: uv add langchain-openai
+    from langchain_openai import ChatOpenAI
+
+    llm = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4o-mini",
+        max_completion_tokens=100,
+    )
+
     prompt = "Three reasons for using LangChain for LLM application development."
 
     response = llm.invoke(input=prompt)
@@ -20,5 +23,35 @@ def openai_models_in_langchain():
     print(response.content)
 
 
+# The standardized syntax that LangChain offers means that models can be
+# quickly changed in and out as new ones are released and tested.
+# Let's see this in action by trying a model from Hugging Face!
+
+
+def huggingface_models_in_langchain():
+    # Install: uv add langchain-huggingface
+    from langchain_huggingface import HuggingFacePipeline
+
+    # Install: transformers to use this: 'uv add transformers'
+    # Install: uv add torch - to install PyTorch
+    # Please note that you may need to restart your runtime after installation.
+    llm = HuggingFacePipeline.from_model_id(
+        model_id="crumb/nano-mistral",
+        task="text-generation",
+        pipeline_kwargs={"max_new_tokens": 20},
+    )
+
+    prompt = "Hugging Face is"
+
+    response = llm.invoke(input=prompt)
+
+    print(response)
+
+
+# This syntax for defining and invoking LLMs unlocks the ability to use almost
+# any model! Now that you have nailed the defining and invoking LLM workflow,
+# let's look at strategies for prompting these models effectively.
+
 if __name__ == "__main__":
-    openai_models_in_langchain()
+    # openai_models_in_langchain()
+    huggingface_models_in_langchain()
