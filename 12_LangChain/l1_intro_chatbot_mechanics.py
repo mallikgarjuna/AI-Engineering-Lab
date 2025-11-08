90fb162c09685d5f518cfc7234ee5e599b8a6ae8
Mallik Gurram <mallikgarjuna@gmail.com>
2025-11-08 12:47:59 +0100

    AIEng/2-LangChain - l1 intro and chatbot mechanics - creating examples set for few shot prompting

--- 12_LangChain/l1_intro_chatbot_mechanics.py
+++ 12_LangChain/l1_intro_chatbot_mechanics.py
@@ -124,8 +124,38 @@ def chat_prompt_templates():
 # head on over to the next video to learn how to scale-up your prompts for larger
 # numbers of examples!
 
+
+# PromptTemplate and ChatPromptTemplate are great for integrating variables,
+# but struggle with integrating datasets containing many examples.
+# This is where FewShotPromptTemplate comes in!
+def creating_few_shot_example_set():
+    # create the examples list of dicts
+    examples = [
+        {
+            "question": "How many DataCamp courses has Jack completed?",
+            "answer": "36",
+        },
+        {
+            "question": "How much XP does Jack have on DataCamp?",
+            "answer": "284,320XP",
+        },
+        {
+            "question": "What technology does Jack learn about most on DataCamp?",
+            "answer": "Python",
+        },
+    ]
+
+    print(f"Examples set: {examples}")
+
+    return examples
+
+
+# With your examples dataset all set up,
+# you're ready to create your few-shot prompt template!
+
 if __name__ == "__main__":
     # openai_models_in_langchain()
     # huggingface_models_in_langchain()
     # prompt_templates_and_chaining()
-    chat_prompt_templates()
+    # chat_prompt_templates()
+    creating_few_shot_example_set()
