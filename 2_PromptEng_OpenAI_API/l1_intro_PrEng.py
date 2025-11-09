dd4d28286e6cb21f6a67396e19caa853b4f3d4ca
Mallik Gurram <mallikgarjuna@gmail.com>
2025-11-09 06:03:54 +0100

    AIEng/2-PromptEng - l1 intro PrEng - create get_response() method

--- 2_PromptEng_OpenAI_API/l1_intro_PrEng.py
+++ 2_PromptEng_OpenAI_API/l1_intro_PrEng.py
@@ -29,5 +29,43 @@ def openai_api_message_roles():
 # Understanding these roles is vital for crafting prompts that harness
 # the OpenAI API's full potential.
 
+
+def get_response(prompt):
+    import os
+
+    from dotenv import load_dotenv
+    from openai import OpenAI
+
+    load_dotenv()
+
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    # Create a request to the chat completions endpoint
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}],
+        max_completion_tokens=200,
+        temperature=0,
+    )
+
+    return response.choices[0].message.content
+
+
+# You've created a function to make your life easier in the following exercises.
+# You'll only need to write one line of code to receive a response from the API.
+
+
+def exploring_prompt_engineering():
+    prompt = (
+        "Generate a poem about ChatGPT in basic English that a child can understand."
+    )
+
+    response = get_response(prompt)
+
+    print(response)
+
+
 if __name__ == "__main__":
-    openai_api_message_roles()
+    # openai_api_message_roles()
+    # print(get_response("What is the capital of France?"))
+    exploring_prompt_engineering()
