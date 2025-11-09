1ef6f72d2a5e2a5de1af423a7ec64a0919b0fb70
Mallik Gurram <mallikgarjuna@gmail.com>
2025-11-09 11:00:38 +0100

    AIEng/4-HuggingFace - l2 pipelines HF - QNLI pipeline

--- 4_HuggingFace/l2_pipelines_HF.py
+++ 4_HuggingFace/l2_pipelines_HF.py
@@ -23,6 +23,25 @@ def grammatical_correctness():
 # The grammar_checker pipeline classified the input text as "LABEL_0", or unacceptable grammar. Now, let's move on to Question Natural Language Inference!
 
 
+def qnli_pipeline():
+    from transformers import pipeline
+
+    classifier = pipeline(
+        task="text-classification",
+        model="cross-encoder/qnli-electra-base",
+    )
+
+    output = classifier(
+        inputs="Where is the capital of France?, Brittany is known for its stunning coastline.",
+    )
+
+    print(output)
+    # [{'label': 'LABEL_0', 'score': 0.016211949288845062}]
+
+
+# the QNLI pipeline showed that the text could not answer the question with a low score. Let's move on to dynamic category assignment.
+
+
 if __name__ == "__main__":
-    grammatical_correctness()
-    # qnli_pipeline()
+    # grammatical_correctness()
+    qnli_pipeline()
